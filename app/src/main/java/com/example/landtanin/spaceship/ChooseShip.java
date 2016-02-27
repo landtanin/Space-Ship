27feaa81a7131fb8f408d7e1b05c5894fe1957b1
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-02-27 23:06:22 +0000

    add layout single_row

--- app/src/main/java/com/example/landtanin/spaceship/ChooseShip.java
+++ app/src/main/java/com/example/landtanin/spaceship/ChooseShip.java
@@ -1,12 +1,16 @@
 package com.example.landtanin.spaceship;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 public class ChooseShip extends AppCompatActivity {
 
     private ListView shipListView;
+    String[] shipName;
+    int[] shipImages;
+    //String[] shipDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,25 @@ public class ChooseShip extends AppCompatActivity {
         bindWidget();
 
         createListView();
+
+        resources();
+
+    }
+
+    private void resources() {
+
+        Resources res = getResources();
+        shipName = res.getStringArray(R.array.Titles);
+
+        shipImages = new int[]{
+
+                R.drawable.falcon_topview,R.drawable.falcon_topview,
+                R.drawable.falcon_topview,R.drawable.falcon_topview,
+                R.drawable.falcon_topview,R.drawable.falcon_topview,
+                R.drawable.falcon_topview,R.drawable.falcon_topview,
+                R.drawable.falcon_topview,R.drawable.falcon_topview
+
+        };
 
     }
 
