76e1c999bfa9ff57a75890f066d99fa32468211b
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-02-27 22:50:00 +0000

    add string data for listview

--- app/src/main/java/com/example/landtanin/spaceship/ChooseShip.java
+++ app/src/main/java/com/example/landtanin/spaceship/ChooseShip.java
@@ -2,12 +2,32 @@ package com.example.landtanin.spaceship;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ChooseShip extends AppCompatActivity {
+
+    private ListView shipListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_ship);
+
+        // Bind Widget
+        bindWidget();
+
+        createListView();
+
+    }
+
+    private void createListView() {
+
+
+
+    }
+
+    private void bindWidget() {
+
+        shipListView = (ListView) findViewById(R.id.shiplistView);
     }
 }
